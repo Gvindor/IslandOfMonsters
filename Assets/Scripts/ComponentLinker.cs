d5cdcc8dee1d0aa6426f880225c43d7e0132b9e7
Vitalii M <mail@inkwalker.xyz>
2021-11-29 08:39:50 +0200

    Kill trigger

--- Assets/Scripts/ComponentLinker.cs
+++ Assets/Scripts/ComponentLinker.cs
@@ -5,8 +5,10 @@ namespace SF
 {
     public class ComponentLinker : MonoBehaviour
     {
+        [SerializeField] FighterCharacterController characterController;
         [SerializeField] CombatController combatController;
 
+        public FighterCharacterController CharacterController => characterController;
         public CombatController CombatController => combatController;
     }
 }
---
+++ Assets/Scripts/KillTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SF
+{
+    public class KillTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            var linker = other.GetComponentInParent<ComponentLinker>();
+
+            if (linker)
+            {
+                linker.CharacterController.Die();
+            }
+        }
+    }
+}
