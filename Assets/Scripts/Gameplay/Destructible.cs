69e9747056d8a23b66fe1d09858e291f0a4a2723
Vitalii M <mail@inkwalker.xyz>
2022-02-01 04:14:36 +0200

    Destructions on Scene_3

---
+++ Assets/Scripts/Gameplay/Destructible.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace SF
+{
+    public class Destructible : MonoBehaviour
+    {
+        [SerializeField] float minImpulse = 50;
+        [SerializeField] Destructible[] chainTrigger;
+        [SerializeField] GameObject[] deactivateOnTrigger;
+
+        private Rigidbody rb;
+
+        public bool IsDestroyed => !rb.isKinematic;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (IsDestroyed) return;
+
+            if (collision.impulse.magnitude > minImpulse)
+                Trigger();
+        }
+
+        public void Trigger()
+        {
+            rb.isKinematic = false;
+
+            foreach (var item in chainTrigger)
+            {
+                item.Trigger();
+            }
+
+            foreach (var item in deactivateOnTrigger)
+            {
+                item.SetActive(false);
+            }
+        }
+    }
+}
